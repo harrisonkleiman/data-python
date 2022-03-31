b3a9557d52052c8e8fdc9db66d1a26cff59ae2c2
Harrison Kleiman <harrikleiman@gmail.com>
2022-03-31 14:47:17 -0400

    lab complete

---
+++ data_presenter.py
@@ -0,0 +1,41 @@
+# Loop through data from CupcakeInvoices.csv and print each row.
+cupcake_file = open("CupcakeInvoices.csv")
+
+for row in cupcake_file: 
+    print(row)
+
+# Loop through all the data and print out the total for each invoice
+# you will need to convert it to a float
+for total in cupcake_file:
+    total = float(total)
+    print(total)
+     
+
+# Loop through all the data, and print out the total for all invoices combined.
+
+for totalInvoice in cupcake_file:
+    totalInvoice = float(totalInvoice)
+    total = total + totalInvoice
+    print(total)
+
+
+# Display the total income of chocolate cupcakes vs vanilla cupcakes vs strawberry cupcakes.
+chocolate_income = 0
+vanilla_income = 0
+strawberry_income = 0
+
+for income in cupcake_file:
+    income = float(income)
+    if "Chocolate" in income:
+        chocolate_income = chocolate_income + income
+    elif "Vanilla" in income:
+        vanilla_income = vanilla_income + income
+    elif "Strawberry" in income:
+        strawberry_income = strawberry_income + income
+    else:
+        print("Not found")
+
+    print(chocolate_income, vanilla_income, strawberry_income)
+
+
+cupcake_file.close()
